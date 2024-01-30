342604571b1beccf51f2766d712be2703d6c4bd0
SAMANTA1401 <105944619+SAMANTA1401@users.noreply.github.com>
2024-01-30 20:28:42 +0530

    data ingestion added

--- main.py
+++ main.py
@@ -1,4 +1,20 @@
 # from src.cnnClassifier import logger # it is also ok
-from cnnClassifier import logger
+# from cnnClassifier import logger
 
-logger.info("welcome to our custom log")
+# logger.info("welcome to our custom log")
+
+from cnnClassifier import logger
+from cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+
+STAGE_NAME = "Data Ingestion stage"
+
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<")
+    data_ingestion = DataIngestionTrainingPipeline()
+    data_ingestion.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed successfully <<<<<\n\nx=======x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+    # 1:37:40
--- src/cnnClassifier/pipeline/stage_01_data_ingestion.py
+++ src/cnnClassifier/pipeline/stage_01_data_ingestion.py
@@ -1,6 +1,6 @@
-from cnnClassifier.config.Configuration import ConfigurationManager
+from cnnClassifier.config.configuration import ConfigurationManager
 from cnnClassifier.components.data_ingestion import DataIngestion
-from cnnClassifier.config.Configuration import ConfigurationManager
+from cnnClassifier.config.configuration import ConfigurationManager
 from cnnClassifier.components.data_ingestion import DataIngestion
 
 from cnnClassifier import logger
@@ -12,5 +12,21 @@ class DataIngestionTrainingPipeline:
         pass
 
 
-def main(self):
-    # 1:33:47
+    def main(self):
+        
+        config = ConfigurationManager()   #make sure that directory is change to "os.chdir("../") " other than  you ge t no such file found
+        data_ingestion_config = config.get_data_ingestion_config()
+        data_ingestion = DataIngestion(config=data_ingestion_config)
+        data_ingestion.download_file()
+        data_ingestion.extract_zip_file()
+
+
+if __name__ == '__main__':
+    try:
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<")
+        obj = DataIngestionTrainingPipeline()
+        obj.main()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed successfully <<<<<\n\nx=======x")
+    except Exception as e:
+        logger.exception(e)
+        raise e
